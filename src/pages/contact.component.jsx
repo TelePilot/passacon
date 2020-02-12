7ac21de6558f4c507e47cf2d88d935073dd0d0c8
blixt <teodor.blixt@gmail.com>
2020-02-12 18:20:50 +0000

    contact responsive

--- src/pages/contact.component.jsx
+++ src/pages/contact.component.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import Title from '../components/title/title.component'
 import sanityClient from '../Client'
 import PortableText from '@sanity/block-content-to-react'
 
@@ -19,6 +18,13 @@ const ContactCont = styled.div`
         height: auto;
     }
 `
+const ContactTitle = styled.h2`
+text-align: left;
+font-size: 48px;
+margin-bottom: 2%;
+@media screen and (max-width: 800px){
+    font-size: 32px;
+}`
 const ContactText = styled.h3`
    
 `
@@ -35,8 +41,16 @@ const MapCont = styled.div`
         width: 100%;
     }
 `
-const ContactTele = styled.h2``
-const ContactLink = styled.a``
+const ContactTele = styled.h3`
+font-size: 28px;
+@media screen and (max-width: 800px){
+    font-size: 22px;
+}
+`
+const ContactLink = styled.a`
+    text-decoration: none;
+    color: black;
+`
 const Contact = () => {
 
     const [contact, setContact] = useState('')
@@ -51,7 +65,7 @@ const Contact = () => {
     }, [])
     return (
         <div>
-        <Title title="Ta kontakt med oss"></Title>
+        <ContactTitle>{contact.titel}</ContactTitle>
         <ContactCont>
                 
                 
@@ -60,12 +74,12 @@ const Contact = () => {
                     
                 <PortableText blocks={contact.beskrivning}/>
             </ContactText>
-            <ContactTele>Telefon:<ContactLink href={`tel:${contact.telefon}`}>{contact.telefon}</ContactLink> </ContactTele>
-            <ContactTele>Email:<ContactLink href={`mailto:${contact.email}`}>{contact.email}</ContactLink> </ContactTele>
+            <ContactTele>Telefon: <ContactLink href={`tel:${contact.telefon}`}>{contact.telefon}</ContactLink> </ContactTele>
+            <ContactTele>Email: <ContactLink href={`mailto:${contact.email}`}>{contact.email}</ContactLink> </ContactTele>
 
                 </ContactTextCont>
                 
-            <MapCont><iframe title="maps" width="100%" height="350" src="https://maps.google.com/maps?width=100%&amp;height=350&amp;hl=en&amp;q=20%2C%20h%C3%B6torget%2C%20111%2057%2C%20stockholm%2C%20sweden+(Passacon)&amp;ie=UTF8&amp;t=&amp;z=17&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">find my location</a></iframe></MapCont>
+            <MapCont><iframe title="maps" width="100%" height="350" src="https://maps.google.com/maps?width=100%&amp;height=400&amp;hl=en&amp;q=Sveav%C3%A4gen%2020%2C%20111%2057%2C%20Stockholm%2C%20Sweden+(Passacon)&amp;ie=UTF8&amp;t=&amp;z=17&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">find my coordinates</a></iframe></MapCont>
         </ContactCont>
         </div>
        
